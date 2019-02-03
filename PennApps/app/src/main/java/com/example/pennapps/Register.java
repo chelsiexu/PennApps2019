1bb94ff2686ac670935d48798185ca053257bb66
HanyuSylvieSun <sunhanyu@seas.upenn.edu>
2019-02-03 12:17:09 -0500

    commit before pull

--- PennApps/app/src/main/java/com/example/pennapps/Register.java
+++ PennApps/app/src/main/java/com/example/pennapps/Register.java
@@ -3,9 +3,18 @@ package com.example.pennapps;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.annotation.NonNull;
 
 public class Register extends AppCompatActivity implements View.OnClickListener{
 
@@ -14,6 +23,8 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
     EditText etPassword;
     EditText etFullname;
     EditText etEmail;
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +38,39 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
         etEmail = (EditText) findViewById(R.id.etEmail);
 
         bRegister.setOnClickListener(this);
-
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.bRegister:
-                String fullname = etFullname.getText().toString();
+                String fullname = etFullname.getText().toString().trim();
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
-                String email = etEmail.getText().toString();
+                String email = etEmail.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(this, "Email can't be blank", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(this, "Password can't be blank", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                firebaseAuth = FirebaseAuth.getInstance();
+                Task<AuthResult> authResultTask = firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Register.this, "Registered Successfully!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(Register.this, "Something's wrong!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
 
                 User user = new User(fullname, username, email, password);
                 break;
