ef21e4da13d2a3502e58d1eed903220580e37b0b
Yitng Hua <h.yt@Ellens-MacBook-Pro.local>
2019-02-03 03:04:45 -0500

    2.3.0304

--- PennApps/app/src/main/java/com/example/pennapps/EditGoals.java
+++ PennApps/app/src/main/java/com/example/pennapps/EditGoals.java
@@ -1,7 +1,9 @@
 package com.example.pennapps;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 public class EditGoals extends AppCompatActivity {
@@ -10,7 +12,5 @@ public class EditGoals extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_goals);
-
-        Button editGoal = (Button)findViewById(R.id.)
     }
 }
