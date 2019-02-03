d42973e7da56cd7ffd50647212b267c181e52e1c
Yitng Hua <h.yt@huntsman-ve501-0540.apn.wlan.upenn.edu>
2019-02-03 12:52:35 -0500

    ellen1252

--- PennApps/app/src/main/java/com/example/pennapps/Map.java
+++ PennApps/app/src/main/java/com/example/pennapps/Map.java
@@ -1,8 +1,15 @@
 package com.example.pennapps;
 
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,7 +17,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Map extends FragmentActivity implements OnMapReadyCallback {
+public class Map extends FragmentActivity implements
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener
+{
 
     private GoogleMap mMap;
 
@@ -38,9 +50,31 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in upenn and move the camera
+        LatLng upenn = new LatLng(39.9539, -75.1930);
+        mMap.addMarker(new MarkerOptions().position(upenn).title("Marker in Upenn"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(upenn));
+        mMap.animateCamera( CameraUpdateFactory.zoomTo( 17.0f ) );
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
